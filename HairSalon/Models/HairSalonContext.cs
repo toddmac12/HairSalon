d49e6a5fb945f4e4a7abc3ecd26e8afeb5275e89
toddmac12 <toddmac12@gmail.com>
2021-10-08 16:48:10 -0400

    edits to database sql file.

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Models
 {
-  public class anthony_mcrae : DbContext
+   public class HairSalonContext : DbContext
   {
     public virtual DbSet<Stylist> Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
