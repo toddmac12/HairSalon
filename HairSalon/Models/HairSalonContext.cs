ae2d5ffef07b5afdb5d294bdb8fabb2d4cbfb703
toddmac12 <toddmac12@gmail.com>
2021-10-08 18:00:48 -0400

    coding added to projects ClientsController file.

--- HairSalon/Models/HairSalonContext.cs
+++ HairSalon/Models/HairSalonContext.cs
@@ -2,11 +2,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Models
 {
-   public class HairSalonContext : DbContext
+  public class HairSalonContext : DbContext
   {
     public DbSet<Stylist> Stylists { get; set; }
     public DbSet<Client> Clients { get; set; }
-    public HairSalonContext(DbContextOptions options) : base(options){ }
+    public HairSalonContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -4,17 +4,17 @@ namespace HairSalon.Models
   public class Stylist
   {
     public override string ToString()
-{
-return $"id:{StylistId} | name:{StylistName} | description:{StylistDescription}";
-}
-public Stylist()
-{
-this.Clients = new HashSet<Client>();
-}
-public int StylistId {get;set;}
-public string StylistName{get;set;}
-public string StylistDescription{get;set;}
-public virtual ICollection<Client> Clients{get;set;}
-}  
+    {
+      return $"id:{StylistId} | name:{StylistName} | description:{StylistDescription}";
+    }
+    public Stylist()
+    {
+      this.Clients = new HashSet<Client>();
+    }
+    public int StylistId { get; set; }
+    public string StylistName { get; set; }
+    public string StylistDescription { get; set; }
+    public virtual ICollection<Client> Clients { get; set; }
+  }
 
 }
